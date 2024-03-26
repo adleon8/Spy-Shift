9431fc5670fb22e8f1b3c9d15e78bce65af8f4df
Weilin471 <wtang1@mail.csuchico.edu>
2024-03-25 21:39:35 -0700

    shooting

--- SpyShift/Assets/Scripts/PlayerController.cs
+++ SpyShift/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,13 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 moveDir;
     private Rigidbody rigid;
     private int speed=5;
-    public float minSpinDistance = 2;
+    public float minSpinDistance;
+    public float minPickUpDistance;
+    private bool hasRock;
+    private bool isShootingRock;
+    private GameObject rock = null;
+
+    private float shootingForce;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +24,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rigid.velocity =transform.TransformDirection(new Vector3(moveDir.x,rigid.velocity.y,moveDir.y));
+        if (isShootingRock)
+        {
+            shootingForce += Time.deltaTime*3;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = transform.TransformDirection(new Vector3(moveDir.x, rigid.velocity.y, moveDir.y));
     }
 
     public void Move(InputAction.CallbackContext input )
@@ -31,20 +45,65 @@ public class PlayerMovement : MonoBehaviour
         moveDir = input.ReadValue<Vector2>()*speed;
     }
 
-    public void Spin()
+    public void Spin(InputAction.CallbackContext input)
     {
         //GameObject e = GameObject.Find("EnemySpawner").GetComponent<EnemySpawner>().GetNearestEnemy();
         //if (Vector3.Distance(transform.position,e.transform.position)<=minSpinDistance)
         //{
         //    GetComponent<MeshRenderer>().material.color = e.GetComponent<MeshRenderer>().material.color;
         //}
-        RaycastHit hit;
-        if( Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out hit,minSpinDistance))
+        if (input.performed)
         {
-            if (hit.transform.gameObject.tag=="Enemy")
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, minSpinDistance))
             {
-                GetComponent<MeshRenderer>().material.color = hit.transform.GetComponent<MeshRenderer>().material.color;
+                if (hit.transform.gameObject.tag == "Enemy")
+                {
+                    GetComponent<MeshRenderer>().material.color = hit.transform.GetComponent<MeshRenderer>().material.color;
+                }
+            }
+        }    
+    }
+
+    public void PickUp(InputAction.CallbackContext input)
+    {
+        
+        if (input.performed)
+        {
+            if (!hasRock)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, minPickUpDistance))
+                {
+                    if (hit.transform.gameObject.tag == "Rock")
+                    {
+                        //     hit.transform.SetParent(transform);
+                        hit.transform.GetComponent<Rock>().SetOnPlayer();
+                        hasRock = true;
+                        rock = hit.transform.gameObject;
+                    }
+                }
+            }
+            else
+            {
+                isShootingRock = true;
+                shootingForce = 0;
+            }        
+        }
+
+        if (input.canceled)
+        {
+            if (isShootingRock)
+            {
+                isShootingRock = false;
+                hasRock = false;
+                rock.transform.GetComponent<Rock>().SetOnPlayer();
+                rock.transform.GetComponent<Rigidbody>().AddRelativeForce(0, shootingForce, shootingForce,ForceMode.Impulse);
+                rock = null;
+                //    transform.Find("Rock").GetComponent<Rock>().Fly(shootingForce);
+                //     transform.Find("Rock").parent = null;
             }
         }
+
     }
 }
